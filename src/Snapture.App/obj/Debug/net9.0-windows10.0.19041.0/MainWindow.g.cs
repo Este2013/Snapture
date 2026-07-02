bf6efab33e1b572ad0c13a3ee978934565cbf9a4
EstebanAragon <esteban.aragon@corsair.com>
2026-07-02 17:27:14 +0200

    release 1.2.0 better notifications for updates

--- src/Snapture.App/obj/Debug/net9.0-windows10.0.19041.0/MainWindow.g.cs
+++ src/Snapture.App/obj/Debug/net9.0-windows10.0.19041.0/MainWindow.g.cs
@@ -548,7 +548,7 @@ namespace Snapture.App {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Snapture;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Snapture;V1.2.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
